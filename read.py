c504ff6045ea54c0161b125b5ab040c868f4f1fe
Diego Duarte <duartqx@gmail.com>
2022-10-29 21:26:40 -0300

    read.py

---
+++ read.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from dbpdlnmpv import DbPdlnMpv
+from sys import argv, exit as _exit
+
+if __name__ == '__main__':
+
+    HELP = '''Missing argument!
+        All of these are required:
+            --db_file arg
+            --table_name arg
+            --id arg
+            --watched arg
+    '''
+
+    if not argv[1:]:
+        print(HELP)
+        _exit(1)
+
+    try:
+        db_file = argv[ argv.index('--db_file') + 1 ]
+        table_name = argv[ argv.index('--table_name') + 1 ]
+        if argv.count('--watched'):
+            watched = argv[ argv.index('--watched') + 1 ]
+        else:
+            watched = 0
+        if argv.count('--id'):
+            _id = argv[ argv.index('--id') + 1 ]
+        else:
+            _id = ''
+    except IndexError:
+        print(HELP)
+        _exit(1)
+    except ValueError as e:
+        print(f'Missing {str(e).split()[0]} argument')
+        _exit(1)
+
+    # Connects to database
+    db = DbPdlnMpv(table_name, db_file)
+
+    # Reads all or just one
+    if _id:
+        db.read_one(_id)
+    else:
+        db.read_all(watched)
+    db.close()
