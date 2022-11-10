0cdb7c6552a164cbf52650bd36c6031611440f4e
Diego Duarte <duartqx@gmail.com>
2022-11-09 23:31:34 -0300

    tweaks to open the file faster

--- dbanimeplaylist.sh
+++ dbanimeplaylist.sh
@@ -18,11 +18,9 @@ case "$Choice" in
     Watch)
         Watch_choice=$(dbmpv $TABLE_NAME --read | dmenu -l 20)
         [[ -z $Watch_choice ]] && exit
-        Filename=${Watch_choice#*- }
-        _id=${Watch_choice%%-*} # Removes everything and leaves only the id
-        cd "$WATCH_FOLDER" || exit
-        mpv --fs "$Filename"
+        mpv --fs "$WATCH_FOLDER/${Watch_choice#*- }"
         # Sets the file to watched
+        _id=${Watch_choice%%-*} # Removes everything and leaves only the id
         dbmpv $TABLE_NAME --update --id "$_id" --watched 1
     ;;
     Update)
