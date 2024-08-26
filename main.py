c5598cb25a7cc622983229be8511a88e9b110218
Diego Duarte <duartqx@gmail.com>
2024-08-26 00:57:18 -0300

    refactor: new main event driven

--- main.py
+++ main.py
@@ -1,22 +1,66 @@
 #!/usr/bin/env python
-from argparse import Namespace
-
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+from sqlite3 import Connection
+from typing import Any, Final
 import asyncio
-import os
 
-from models import get_connection, DbPlMpv
-from handlers.cli import get_cli_args, cli_handler
+from api.controllers import create, index
+from application import bootstrap
+from domain.entities import Anime
+from repository.anime import AnimeRepository
+
+
+Filename = str
+
+DATABASE: Final[Path] = Path.home() / ".local" / "share" / "playlists.db"
+BASEPATH: Final[Path] = Path.home() / "Media" / "Videos"
+
+
+class DbMpvArgs(Namespace):
+    create: list[Filename]
+
+
+def get_options() -> tuple[dict[str, Any], ...]:
+    return (
+        {
+            "arg": ("-c", "--create"),
+            "help": "Create anime entry with the argument passed as title",
+            "action": "store",
+            "nargs": "*",
+        },
+    )
+
+
+def get_args() -> DbMpvArgs:
+    """
+    Builds and returns main's parsed command line arguments
+
+    OPTIONS:
+        -c, --create: Filename
+    """
+    parser = ArgumentParser(prog="DbMpv-cli")
+
+    for arg in get_options():
+        parser.add_argument(*arg.pop("arg"), **arg)
+
+    return parser.parse_args(namespace=DbMpvArgs())
 
 
 async def main() -> None:
-    args: Namespace = get_cli_args()
+    args = get_args()
 
-    config: str = f"{os.environ['HOME']}/.config/dbmpv.json"
-    db: DbPlMpv = get_connection(config)
-
-    with db.conn:
-        await cli_handler(db=db, args=args)
+    with conn:
+        if args.create:
+            for title in args.create:
+                create(Anime(title=title, path=BASEPATH / title))
+        else:
+            index()
 
 
 if __name__ == "__main__":
+    conn = Connection(database=DATABASE)
+
+    bootstrap(repository=AnimeRepository(conn=conn))
+
     asyncio.run(main())
