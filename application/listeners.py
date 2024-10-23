b3c6161ed37f982275ca6784587f232012e1f293
Diego Duarte <duartqx@gmail.com>
2024-10-23 00:27:38 -0300

    fix: updated message empty when not event.anime.watched

--- application/listeners.py
+++ application/listeners.py
@@ -11,12 +11,10 @@ def notify_was_created(event: WasCreated) -> None:
 
 @registry.add(event=WasUpdated)
 def notify_was_updated(event: WasUpdated) -> None:
-    # fmt: off
     notify_send(
-        f"Updated: {event.anime.title}"
-        f" [Watched]" if event.anime.watched else f""
+        f"""Updated: {event.anime.title}"""
+        f"""{" [Watched]" if event.anime.watched else ""}"""
     )
-    # fmt: on
 
 
 @registry.add(event=WereDeleted)
